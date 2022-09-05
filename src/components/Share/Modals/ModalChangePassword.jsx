a944a793ab7e25a3f616ba3be94e4f5f8be61644
ziehuog <bdieuhuong10@gmail.com>
2022-09-05 22:49:40 +0700

    OK

--- src/components/Share/Modals/ModalChangePassword.jsx
+++ src/components/Share/Modals/ModalChangePassword.jsx
@@ -7,49 +7,44 @@ import { toast } from "react-toastify";
 import { db } from "../../../utils/firebase";
 
 function ModalChangePassword(props) {
-    const [password, setPassword] = useState('')
-    const [newPassword, setNewPassword] = useState('')
-    const [cfNewPassword, setCfNewPassword] = useState('')
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [cfNewPassword, setCfNewPassword] = useState("");
 
-    const [message, setMessage] = useState('')
-    const [messagePw, setMessagePw] = useState('')
+  const [message, setMessage] = useState("");
+  const [messagePw, setMessagePw] = useState("");
 
-    const id = props.user?.id
+  const id = props.user?.id;
 
-    console.log(id)
-    
-    const onSubmit = async () => {
+  console.log(id);
 
-        if(password !== props.user?.data.password){
-            setMessage('wrong password!')
-            return;
-        } else {
-            setMessage('')
-        }
-        if(cfNewPassword !== newPassword ){
-            setMessagePw('cf failed')
-            return;
-        } else{
-
-        }
-        try {
-            await updateDoc(doc(db, "Users", id), {
-              ...props.user.data,
-              data: {
-                password: newPassword, 
-                ...props.user.data
-              }
-              
-            });
-            toast.success("update successfully!");
-            props.onHide();
-            window.location.reload();
-          } catch {
-            toast.error("Update failed");
-          }
-
-
+  const onSubmit = async () => {
+    if (password !== props.user?.data.password) {
+      setMessage("wrong password!");
+      return;
+    } else {
+      setMessage("");
     }
+    if (cfNewPassword !== newPassword) {
+      setMessagePw("cf failed");
+      return;
+    } else {
+    }
+    try {
+      await updateDoc(doc(db, "Users", id), {
+        ...props.user.data,
+        data: {
+          password: newPassword,
+          ...props.user.data,
+        },
+      });
+      toast.success("update successfully!");
+      props.onHide();
+      window.location.reload();
+    } catch {
+      toast.error("Update failed");
+    }
+  };
   return (
     <Modal
       {...props}
@@ -63,49 +58,60 @@ function ModalChangePassword(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <label >Password</label>
+        <label>Password</label>
         <div className="flex mb-[15px]">
           <input
             className="h-[35px] w-full bg-gray-100 rounded-md px-[15px] outline-none placeholder:text-gray-500"
             type="password"
             placeholder="password"
             value={password}
-            onChange ={e => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             // ref={refPassword}
-            
           />
         </div>
-          <p className="text-red-500">{message}</p>
+        <p className="text-red-500">{message}</p>
 
-        <label >New Password</label>
+        <label>New Password</label>
         <div className="flex mb-[15px]">
           <input
             className="h-[35px] w-full bg-gray-100 rounded-md px-[15px] outline-none placeholder:text-gray-500"
             type="password"
             placeholder="new password"
             value={newPassword}
-            onChange ={e => setNewPassword(e.target.value)}
+            onChange={(e) => setNewPassword(e.target.value)}
             // onChange
           />
         </div>
 
-        <label >Confirm new password</label>
+        <label>Confirm new password</label>
         <div className="flex mb-[15px]">
           <input
             className="h-[35px] w-full bg-gray-100 rounded-md px-[15px] outline-none placeholder:text-gray-500"
             type="password"
             placeholder="Confirm"
             value={cfNewPassword}
-            onChange ={e => setCfNewPassword(e.target.value)}
-
+            onChange={(e) => setCfNewPassword(e.target.value)}
           />
         </div>
         <p className="text-red-500">{messagePw}</p>
-
       </Modal.Body>
       <Modal.Footer>
-        <button onClick={onSubmit} className="border px-2 py-1">Ok</button>
-        <Button onClick={props.onHide}>Close</Button>
+        <button
+          onClick={onSubmit}
+          className="border transition duration-300 cursor-pointer px-4 py-2 
+              bg-gradient-to-r from-indigo-500 via-indigo-300 to-indigo-200
+               my-[20px] rounded-md hover:bg-sky-700  hover:text-white mx-3"
+        >
+          Ok
+        </button>
+        <button
+          onClick={props.onHide}
+          className="border transition duration-300 cursor-pointer px-4 py-2 
+              bg-gradient-to-r from-indigo-500 via-indigo-300 to-indigo-200
+               my-[20px] rounded-md hover:bg-sky-700  hover:text-white mx-3"
+        >
+          Close
+        </button>
       </Modal.Footer>
     </Modal>
   );
